f77c3f5ce1be16f2f5340534ddd3e04a503cb44a
Liam <liam@nehoray.net>
2025-04-25 14:04:44 +0200

    Github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://caduregel.github.io',
+    base: 'astro-blog',
+});
